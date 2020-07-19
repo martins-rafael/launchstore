5f7cd09871e15968bb58f7a252e320d3963fa098
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-19 20:17:06 -0300

    Products form layout

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const routes = express.Router()
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function (req, res) {
     return res.render('layout')
 })
 
+routes.get('/products/create', ProductController.create)
+
+routes.get('/ads/create', function (req, res) {
+    return res.redirect('/products/create')
+})
 module.exports = routes
