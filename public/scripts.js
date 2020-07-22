24ed2dc2fd0a8e822edaefcbd79b710d25928951
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-21 22:22:40 -0300

    Starting the data structure of the image upload

--- public/scripts.js
+++ public/scripts.js
@@ -15,3 +15,17 @@ const Mask = {
         event.target.value = value;
     }
 };
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const { files: fileList } = event.target;
+        const { uploadLimit } = PhotosUpload;
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos.`);
+            event.preventDefault();
+            return;
+        }
+    }
+};
