7cfed18e13d8a86cf4d5aa99f6e3e65e22bfc75a
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-25 22:05:19 -0300

    Upload manager complete

--- public/scripts.js
+++ public/scripts.js
@@ -17,13 +17,18 @@ const Mask = {
 };
 
 const PhotosUpload = {
+    input: '',
     preview: document.querySelector('#photo-preview'),
     uploadLimit: 6,
+    files: [],
     handleFileInput(event) {
         const { files: fileList } = event.target;
+        PhotosUpload.input = event.target;
+
         if (PhotosUpload.hasLimit(event)) return;
 
         Array.from(fileList).forEach(file => {
+            PhotosUpload.files.push(file);
             const reader = new FileReader();
 
             reader.onload = () => {
@@ -36,9 +41,12 @@ const PhotosUpload = {
 
             reader.readAsDataURL(file);
         });
+
+        PhotosUpload.input.files = PhotosUpload.getAllFiles();
     },
     hasLimit(event) {
-        const { uploadLimit } = PhotosUpload;
+        const { uploadLimit, input, preview } = PhotosUpload;
+        const { files: fileList } = input;
 
         if (fileList.length > uploadLimit) {
             alert(`Envie no máximo ${uploadLimit} fotos.`);
@@ -47,14 +55,51 @@ const PhotosUpload = {
             return true;
         }
 
+        const photosDiv = [];
+        preview.childNodes.forEach(item => {
+            if (item.classList && item.classList.value == 'photo')
+                photosDiv.push(item);
+        });
+
+        const totalPhotos = fileList.length + photosDiv.length;
+        if (totalPhotos > uploadLimit) {
+            alert(`Você atingiu o limite máximo de ${uploadLimit} fotos.`);
+            event.preventDefault();
+            return true;
+        }
+
         return false;
+    },
+    getAllFiles() {
+        const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer();
+        PhotosUpload.files.forEach(file => dataTransfer.items.add(file));
+
+        return dataTransfer.files;
     },
     getContainer(image) {
         const div = document.createElement('div');
         div.classList.add('photo');
-        div.onclick = () => alert('funciona!');
+        div.onclick = PhotosUpload.removePhoto;
         div.appendChild(image);
+        div.appendChild(PhotosUpload.getRemoveButton());
 
         return div;
+    },
+    getRemoveButton() {
+        const button = document.createElement('i');
+        button.classList.add('material-icons');
+        button.innerHTML = 'close';
+
+        return button;
+    },
+    removePhoto(event) {
+        const photoDiv = event.target.parentNode; // <div class="photo">
+        const photosarray = Array.from(PhotosUpload.preview.children);
+        const index = photosarray.indexOf(photoDiv);
+
+        PhotosUpload.files.splice(index, 1);
+        PhotosUpload.input.files = PhotosUpload.getAllFiles();
+
+        photoDiv.remove();
     }
 };
