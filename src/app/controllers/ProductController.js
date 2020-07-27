83784b8e0ed1f1c0a8a2d70a75606da6209fb178
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-27 19:54:46 -0300

    Structuring images to send to Front end

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -44,10 +44,19 @@ module.exports = {
         product.old_price = formatPrice(product.old_price);
         product.price = formatPrice(product.price);
 
+        // get categories
         results = await Category.all();
         const categories = results.rows;
+        let files = results.rows;
+        files.map(file => ({
+            ...file,
+            src:`${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+        }));
 
-        return res.render('products/edit', { product, categories });
+        //get images
+        results = await Product.files(product.id);
+
+        return res.render('products/edit', { product, categories, files });
     },
     async put(req, res) {
         const keys = Object.keys(req.body);
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -62,5 +62,9 @@ module.exports = {
     },
     delete(id) {
         return db.query(`DELETE FROM products WHERE id = $1`, [id]);
+    },
+    files(id) {
+        return db.query(`
+        SELECT * FROM files WHERE product_id = $1`, [id]);
     }
 };
