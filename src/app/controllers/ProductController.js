e68c0faa0183467ead20bce54c03094cc9e24b08
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-29 15:10:31 -0300

    Product data presentation

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,4 @@
-const { formatPrice } = require('../../lib/utils');
+const { formatPrice, date } = require('../../lib/utils');
 const Category = require('../models/Category');
 const Product = require('../models/Product');
 const File = require('../models/File');
@@ -36,8 +36,21 @@ module.exports = {
 
         return res.redirect(`/products/${productId}`);
     },
-    show(req, res) {
-        return res.render('products/show');
+    async show(req, res) {
+        let results = await Product.find(req.params.id);
+        const product = results.rows[0];
+
+        if (!product) return res.send('Produto não encontrado!');
+
+        const { minutes, hour, day, month } = date(product.updated_at);
+        product.published = {
+            hour: `${hour}h${minutes}`,
+            day: `${day}/${month}`,
+        };
+        product.old_price = formatPrice(product.old_price);
+        product.price = formatPrice(product.price);
+
+        return res.render('products/show', { product });
     },
     async edit(req, res) {
         let results = await Product.find(req.params.id);
@@ -96,7 +109,7 @@ module.exports = {
 
         await Product.update(req.body);
 
-        return res.redirect(`products/${req.body.id}/edit`)
+        return res.redirect(`products/${req.body.id}`)
     },
     async delete(req, res) {
         await Product.delete(req.body.id);
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,11 +1,15 @@
 module.exports = {
     date(timestamp) {
         const date = new Date(timestamp);
-        const year = date.getUTCFullYear();
-        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
-        const day = `0${date.getUTCDate()}`.slice(-2);
+        const year = date.getFullYear();
+        const month = `0${date.getMonth() + 1}`.slice(-2);
+        const day = `0${date.getDate()}`.slice(-2);
+        const hour = date.getHours();
+        const minutes = `0${date.getMinutes()}`.slice(-2);
 
         return {
+            minutes,
+            hour,
             day,
             month,
             year,
