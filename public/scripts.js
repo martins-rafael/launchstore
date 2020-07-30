bf43ce74412e22a8ec636d829198c1c3d6a60457
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-29 21:55:14 -0300

    Image gallery features

--- public/scripts.js
+++ public/scripts.js
@@ -115,3 +115,14 @@ const PhotosUpload = {
         photoDiv.remove();
     }
 };
+
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    setImage(event) {
+        const { target } = event;
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'));
+        target.classList.add('active');
+        ImageGallery.highlight.src = target.src;
+    }
+}
