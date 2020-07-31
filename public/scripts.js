dab019fd4c04b2ddbdc8384c6bbd15a1539129e8
Rafael Martins <rafaeldcmartins@gmail.com>
2020-07-31 14:42:27 -0300

    Lightbox JavaScript

--- public/scripts.js
+++ public/scripts.js
@@ -104,8 +104,8 @@ const PhotosUpload = {
     },
     removeOldPhoto(event) {
         const photoDiv = event.target.parentNode;
-        
-        if (photoDiv.id){
+
+        if (photoDiv.id) {
             const removedFiles = document.querySelector('input[name="removed_files"]');
             if (removedFiles) {
                 removedFiles.value += `${photoDiv.id},`;
@@ -124,5 +124,24 @@ const ImageGallery = {
         ImageGallery.previews.forEach(preview => preview.classList.remove('active'));
         target.classList.add('active');
         ImageGallery.highlight.src = target.src;
+        Lightbox.image.src = target.src;
     }
-}
+};
+
+const Lightbox = {
+    target: document.querySelector('.lightbox-target'),
+    image: document.querySelector('.lightbox-target img'),
+    closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+    open() {
+        Lightbox.target.style.opacity = 1;
+        Lightbox.target.style.top = 0;
+        Lightbox.target.style.bottom = 0;
+        Lightbox.closeButton.style.top = 0;
+    },
+    close() {
+        Lightbox.target.style.opacity = 0;
+        Lightbox.target.style.top = '-100%';
+        Lightbox.target.style.bottom = 'initial';
+        Lightbox.closeButton.style.top = '-80px';
+    }
+};
