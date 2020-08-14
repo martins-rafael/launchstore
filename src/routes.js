4e8fd770a2d0dca6bf278f9d8d20917b7f28a09c
Rafael Martins <rafaeldcmartins@gmail.com>
2020-08-13 21:52:45 -0300

    Home HTML structure

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const multer = require('./app/middlewares/multer');
 const ProductController = require('./app/controllers/ProductController');
 
 routes.get('/', function (req, res) {
-    return res.render('layout');
+    return res.render('home/index');
 });
 
 routes.get('/products/create', ProductController.create);
