960dfb3763f6498afc242214e39129aa36800030
Rafael Martins <rafaeldcmartins@gmail.com>
2020-08-14 10:58:23 -0300

    Structuring data for the frond end

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -8,5 +8,26 @@ module.exports = {
         const products = results.rows;
 
         if (!products) return res.send('Produtos não encontrados!');
+
+        async function getImage(productId) {
+            let results = await Product.files(productId);
+            const files = results.rows.map(file => {
+                return `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`;
+            });
+
+            return files[0];
+        }
+
+        const productsPromise = products.map(async product => {
+            product.img = await getImage(product.id);
+            product.oldPrice = formatPrice(product.old_price);
+            product.price = formatPrice(product.price);
+
+            return product;
+        }).filter((product, index) => index > 2 ? false : true);
+
+        const lastAdded = await Promise.all(productsPromise);
+
+        return res.render('home/index', { products: lastAdded })
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const multer = require('./app/middlewares/multer');
 const ProductController = require('./app/controllers/ProductController');
+const HomeController = require('./app/controllers/HomeController');
 
-routes.get('/', function (req, res) {
-    return res.render('home/index');
-});
+routes.get('/', HomeController.index);
 
 routes.get('/products/create', ProductController.create);
 routes.get('/products/:id', ProductController.show);
