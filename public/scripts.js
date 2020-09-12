427a911bcb05a90a988ad5165660de68f07c963e
Rafael Martins <rafaeldcmartins@gmail.com>
2020-09-11 22:35:25 -0300

    Show email validation

--- public/scripts.js
+++ public/scripts.js
@@ -45,17 +45,30 @@ const Mask = {
 
 const Validate = {
     apply(input, func) {
+        Validate.clearErrors(input);
+
         let results = Validate[func](input.value);
         input.value = results.value;
 
-        if (results.error) alert(results.error);
+        if (results.error) Validate.displayError(input, results.error);
+    },
+    displayError(input, error) {
+        const div = document.createElement('div');
+        div.classList.add('error');
+        div.innerHTML = error;
+        input.parentNode.appendChild(div);
 
+        input.focus();
+    },
+    clearErrors(input) {
+        const errorDiv = input.parentNode.querySelector('.error');
+        if (errorDiv) errorDiv.remove();
     },
     isEmail(value) {
         let error = null;
         const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-        if (!value.match(mailFormat)) error = 'Email invalido!';
+        if (!value.match(mailFormat)) error = 'Email inválido';
 
         return {
             error,
