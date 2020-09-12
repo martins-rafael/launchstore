2f601aaa8065eaacc25536d8c6080264cc8574c3
Rafael Martins <rafaeldcmartins@gmail.com>
2020-09-11 21:13:28 -0300

    CPF, CNPJ and CEP Mask

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@ const Mask = {
     apply(input, func) {
         setTimeout(function () {
             input.value = Mask[func](input.value);
-        });
+        }, 1);
     },
     formatBRL(value) {
         value = value.replace(/\D/g, '');
@@ -11,8 +11,35 @@ const Mask = {
             style: 'currency',
             currency: 'BRL'
         }).format(value / 100);
+    },
+    cpfCnpj(value) {
+        value = value.replace(/\D/g, '');
 
-        event.target.value = value;
+        // check if cnpj - 11.222.333/0001-11
+        if (value.length > 11) {
+            if (value.length > 14) value = value.slice(0, -1);
+
+            value = value.replace(/(\d{2})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1/$2');
+            value = value.replace(/(\d{4})(\d)/, '$1-$2');
+        } else {
+            // cpf 111.222.333-44
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1-$2');
+        }
+
+        return value;
+    },
+    cep(value) {
+        value = value.replace(/\D/g, '');
+
+        if (value.length > 8) value = value.slice(0, -1);
+
+        value = value.replace(/(\d{5})(\d)/, '$1-$2');
+
+        return value;
     }
 };
 
