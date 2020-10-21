921e0a33a39390fcfe9cac51d3ac8459941ab7ce
Rafael Martins <rafaeldcmartins@gmail.com>
2020-10-21 09:23:53 -0300

    Search adjustments

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -4,35 +4,28 @@ const LoadService = require('../services/LoadProductService');
 module.exports = {
     async index(req, res) {
         try {
-            let params = {};
-            const { filter, category } = req.query;
+            let { filter, category } = req.query;
 
-            if (!filter) return res.redirect('/');
+            if (!filter || filter.toLowerCase() == 'toda a loja') filter = null;
 
-            params.filter = filter;
-
-            if (category) {
-                params.category = category;
-            }
-
-            let products = await Product.search(params);
+            let products = await Product.search({ filter, category });
             const productsPromise = products.map(LoadService.format);
-            
+
             products = await Promise.all(productsPromise);
-            
+
             const search = {
-                term: req.query.filter,
+                term: filter || 'Toda a loja',
                 total: products.length
             };
 
-            const categories = products.map(product=>({
+            const categories = products.map(product => ({
                 id: product.category_id,
                 name: product.category_name
             })).reduce((categoriesFiltered, category) => {
                 const found = categoriesFiltered.some(cat => cat.id == category.id);
                 if (!found) categoriesFiltered.push(category);
                 return categoriesFiltered
-            },[]);
+            }, []);
 
             return res.render('search/index', { products, search, categories });
         } catch (err) {
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -11,31 +11,26 @@ module.exports = {
 
         return results.rows;
     },
-    async search(params) {
-        const { filter, category } = params;
-        let query = '',
-            filterQuery = `WHERE`;
+    async search({ filter, category }) {
+        let query = `
+            SELECT products.*,
+            categories.name AS category_name
+            FROM products
+            LEFT JOIN categories ON (categories.id = products.category_id)
+            WHERE 1 = 1`;
 
         if (category) {
-            filterQuery = `
-            ${filterQuery}
-            (products.category_id = ${category} AND products.name ILIKE '%${filter}%')
-            OR (products.category_id = ${category} AND products.description ILIKE '%${filter}%')`;
+            query += `
+            AND products.category_id = ${category}`;
         }
 
-        if (!category) {
-            filterQuery = `
-            ${filterQuery}
-            products.name ILIKE '%${filter}%'
-            OR products.description ILIKE '%${filter}%'`;
+        if (filter) {
+            query += `
+            AND (products.name ILIKE '%${filter}%'
+            OR products.description ILIKE '%${filter}%')`;
         }
 
-        query = `
-        SELECT products.*,
-        categories.name AS category_name
-        FROM products
-        LEFT JOIN categories ON (categories.id = products.category_id)
-        ${filterQuery}`;
+        query += `AND status != 0`;
 
         const results = await db.query(query);
         return results.rows;
