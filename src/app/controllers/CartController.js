84dca05b81af59cbefb6175051910087b6699218
Rafael Martins <rafaeldcmartins@gmail.com>
2020-11-14 20:17:02 -0300

    Add and remove items from cart

--- src/app/controllers/CartController.js
+++ src/app/controllers/CartController.js
@@ -4,15 +4,35 @@ const loadProductsService = require('../services/LoadProductService');
 module.exports = {
     async index(req, res) {
         try {
-            const product = await loadProductsService.load('product', { where: { id: 12 } });
             let { cart } = req.session;
 
             // cart manager
-            cart = Cart.init(cart).addOne(product);
+            cart = Cart.init(cart);
 
             return res.render('cart/index', { cart });
         } catch (err) {
             console.log(err);
         }
+    },
+    async addOne(req, res) {
+        const { id } = req.params;
+        const product = await loadProductsService.load('product', { where: { id } });
+
+        let { cart } = req.session;
+        cart = Cart.init(cart).addOne(product);
+        req.session.cart = cart;
+
+        return res.redirect('/cart');
+
+    },
+    async removeOne(req, res) {
+        const { id } = req.params;
+
+        let { cart } = req.session;
+        if (!cart) return res.redirect('/cart');
+        cart = Cart.init(cart).removeOne(id);
+
+        req.session.cart = cart;
+        return res.redirect('/cart');
     }
 };
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,5 +4,7 @@ const routes = express.Router();
 const CartController = require('../app/controllers/CartController');
 
 routes.get('/', CartController.index);
+routes.post('/:id/add-one', CartController.addOne);
+routes.post('/:id/remove-one', CartController.removeOne);
 
 module.exports = routes;
