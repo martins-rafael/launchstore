e269c4cfb39932f83bf0fbfce605c125de855ccc
Rafael Martins <rafaeldcmartins@gmail.com>
2021-05-11 08:39:09 -0300

    Update middlewares

--- src/app/middlewares/multer.js
+++ src/app/middlewares/multer.js
@@ -1,26 +1,26 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now().toString()}-${file.originalname}`);
-    }
+  destination: (req, file, cb) => {
+    cb(null, './public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now().toString()}-${file.originalname}`);
+  }
 });
 
 const fileFilter = (req, file, cb) => {
-    const isAccepted = ['image/png', 'image/jpg', 'image/jpeg']
-        .find(acceptedFormat => acceptedFormat == file.mimetype);
+  const isAccepted = ['image/png', 'image/jpg', 'image/jpeg']
+    .find(acceptedFormat => acceptedFormat == file.mimetype);
 
-    if (isAccepted) {
-        return cb(null, true);
-    }
+  if (isAccepted) {
+    return cb(null, true);
+  }
 
-    return cb(null, false);
+  return cb(null, false);
 };
 
 module.exports = multer({
-    storage,
-    fileFilter
+  storage,
+  fileFilter
 });
--- src/app/validators/product.js
+++ src/app/validators/product.js
@@ -1,32 +1,32 @@
 async function post(req, res, next) {
-    const keys = Object.keys(req.body);
+  const keys = Object.keys(req.body);
 
-    for (let key of keys) {
-        if (req.body[key] == '') {
-            return res.send('Por favor, volte e preencha todos os campos.');
-        }
+  for (let key of keys) {
+    if (req.body[key] == '') {
+      return res.send('Por favor, volte e preencha todos os campos.');
     }
+  }
 
-    if (!req.files || req.files.length == 0) {
-        return res.send('Por favor, envie pelo menos uma imagem.')
-    }
+  if (!req.files || req.files.length == 0) {
+    return res.send('Por favor, envie pelo menos uma imagem.');
+  }
 
-    next();
+  next();
 }
 
 async function put(req, res, next) {
-    const keys = Object.keys(req.body);
+  const keys = Object.keys(req.body);
 
-    for (let key of keys) {
-        if (req.body[key] == '' && key != 'removed_files') {
-            return res.send('Por favor, volte e preencha todos os campos.');
-        }
+  for (let key of keys) {
+    if (req.body[key] == '' && key != 'removed_files') {
+      return res.send('Por favor, volte e preencha todos os campos.');
     }
+  }
 
-    next();
+  next();
 }
 
 module.exports = {
-    post,
-    put
+  post,
+  put
 };
