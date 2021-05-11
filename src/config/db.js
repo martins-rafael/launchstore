62f586eb4beec72f28ff4112b2efc1f80a6bee9e
Rafael Martins <rafaeldcmartins@gmail.com>
2021-05-11 08:36:18 -0300

    Update config and lib

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 module.exports = new Pool({
-    user: 'postgres',
-    password: '0000',
-    host: 'localhost',
-    port: 5432,
-    database: 'launchstoredb'
+  user: 'postgres',
+  password: 'senha',
+  host: 'localhost',
+  port: 5432,
+  database: 'launchstore'
 });
--- src/config/session.js
+++ src/config/session.js
@@ -3,13 +3,13 @@ const pgSession = require('connect-pg-simple')(session);
 const db = require('./db');
 
 module.exports = session({
-    store: new pgSession({
-        pool: db
-    }),
-    secret: 'biscoit0',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 30 * 24 * 60 * 60 * 1000
-    }
+  store: new pgSession({
+    pool: db
+  }),
+  secret: 'biscoit0',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  }
 });
