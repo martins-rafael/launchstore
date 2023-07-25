48c39a076be6326637a026a15f03b4b2ca7dd567
flavio <flavio_cetecci@hotmail.com>
2023-07-25 18:37:07 -0300

    Aplicacao teste

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 module.exports = new Pool({
   user: 'postgres',
-  password: 'senha',
+  password: 'password',
   host: 'localhost',
   port: 5432,
   database: 'launchstore'
